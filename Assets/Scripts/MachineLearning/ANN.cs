fbaa9fbb2e57da737099ff06e65a48f02689fa04
Tyler Baylson <thbaylson@gmail.com>
2022-04-14 22:08:55 -0400

    Added MachineLearning namespace.

---
+++ Assets/Scripts/MachineLearning/ANN.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace RPG.MachineLearning
+{
+    public class ANN : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
