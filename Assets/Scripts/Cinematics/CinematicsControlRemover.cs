cfbe9245b6fe1d58e1584dd49b8e87db29f970bc
Tyler Baylson <thbaylson@gmail.com>
2022-04-19 23:08:54 -0400

    Added a trigger for the cinematic sequence. Added FakePlayableDirector class, but this is just to be an example of delagate functions and will be deleted later.

---
+++ Assets/Scripts/Cinematics/CinematicsControlRemover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Cinematics
+{
+    public class CinematicsControlRemover : MonoBehaviour
+    {
+        private void Start()
+        {
+            // Notice that these are called in the exact order that they are added
+            GetComponent<FakePlayableDirector>().onFinish += EnableControl;
+            GetComponent<FakePlayableDirector>().onFinish += DisableControl;
+        }
+
+        void DisableControl(float nonsenseFloat)
+        {
+            print("DisableControl");
+        }
+
+        // This needs a float because the event Action<> was declared to require
+        void EnableControl(float nonsenseFloat)
+        {
+            print("EnableControl");
+        }
+    }
+}
