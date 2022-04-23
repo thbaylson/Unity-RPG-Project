400d9bc9ba51b629db5b75e88eeca434c244501d
Tyler Baylson <thbaylson@gmail.com>
2022-04-22 22:29:54 -0400

    Added RicksSandbox2 scene. Added scene transition functionality.

---
+++ Assets/Scripts/SceneManagement/SceneTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class SceneTrigger : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad = -1;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            // DontDestroyOnLoad only works if the gameObject is in the root of the scene (not childed)
+            DontDestroyOnLoad(gameObject);
+
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            print($"Loaded Build Index Scene: {sceneToLoad}");
+            Destroy(gameObject);
+        }
+    }
+}
